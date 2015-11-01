e5cd8b52480d5514a90438099fd5d96053e9f4ff
Ryan Neufeld <ryan@neufeldmail.com>
2015-11-01 11:32:21 -0800

    The first commit's message is:
    Updaing for my env.
    
    My version of rtorrent config
    Mounting .rtorrent session dir outside of the container

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run -dt --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 49160:49160/udp -p 49161:49161 -v ~/test/rtorrent:/downloads diameter/rtorrent-rutorrent:64-latest
-
+docker run -dt --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 25001-25019:25001-25019 -v /data/dropbox/Dropbox/Torrents:/torrents -v /data/downloads:/downloads -v /data/rtorrent/.rtorrent:/torrents/.rtorrent diameter/rtorrent-rutorrent:64-latest
