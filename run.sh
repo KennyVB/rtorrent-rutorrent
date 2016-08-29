bd11fa59f9b644f0b6c98d1cbbec9549a6b86163
Ryan Neufeld <ryan@neufeldmail.com>
2016-08-28 22:05:50 -0700

    updating configs

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -dt --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 25001-25019:25001-25019 -v /data/dropbox/Dropbox/Torrents:/torrents -v /data/downloads:/downloads -v /data/rtorrent/.rtorrent:/torrents/.rtorrent diameter/rtorrent-rutorrent:64-latest
+docker run -dt --restart always --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 25001-25019:25001-25019/udp -p 25001-25019:25001-25019 -v /data/dropbox/Dropbox/Torrents:/torrents -v /data/downloads:/downloads -v /data/rtorrent/.rtorrent:/torrents/.rtorrent diameter/rtorrent-rutorrent:64-latest
